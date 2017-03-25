99d3123e7daf254352e5127841fdc36248c6aba3
Gabriel Pereira <gsilvap1994@gmail.com>
2017-03-25 14:28:40 -0300

    realizada integração com google maps

--- app.rb
+++ app.rb
@@ -4,10 +4,11 @@ require 'sinatra'
 require 'httparty'
 
 get '/' do
-  endereco = params['url'].gsub(' ', '+')
-  escaped  = URI.escape(endereco)
-  url      = "http://maps.google.com/maps/api/geocode/json?address=#{escaped}"
-  response = HTTParty.get(url)
-  parsed   = JSON.parse(response.body)['results']
-  erb :index, :locals => {result: parsed}
+  if params('url')
+    escaped  = URI.escape(endereco)
+    url      = "http://maps.google.com/maps/api/geocode/json?address=#{escaped}"
+    response = HTTParty.get(url)
+    parsed   = JSON.parse(response.body)['results']
+    erb :index, :locals => {result: parsed}
+  end
 end
